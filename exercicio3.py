032a966419da599e4a70db8fe1cad5d00a8d4be6
Matheus Borges <matheusbitcoin123@gmail.com>
2024-09-11 21:00:20 -0300

    Create exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,12 @@
+print("Qual é o valor da variável soma?")
+# Define o valor das variáveis
+indice = 12
+soma = 0
+k = 1
+# Cria um looping, adicionando valores até o necessário
+while k < indice:
+    k = k + 1
+    soma = soma + k
+
+print (f"O valor da variável soma é {soma}")
+
