fb0071ae204a37aa8aad676436e9aef71faf1a8c
Matheus Borges <matheusbitcoin123@gmail.com>
2024-09-11 22:50:40 -0300

    Create exercicio5.py

---
+++ exercicio5.py
@@ -0,0 +1,6 @@
+# Vamos dar nomes aos interruptores, sendo eles, A, B e C.
+# 1° Vamos ligar os interruptores A e B e deixá-los ligados tempo suficiente para aquecer as lâmpadas
+# 2° Vamos desligar o interruptor B
+# 3° Após isso, vamos seguir para a primeira sala, caso ela esteja com a lâmpada acesa, vamos descobrir que é pertencente ao interruptor A
+# Após isso, vamos seguir para a próxima sala, que vai estar com a lâmpada apagada, vamos verificar a temperatura da lâmpada e caso ela esteja quente, indica que é pertencente ao interruptor B, caso esteja fria, ao interruptor C
+# Mas caso a primeira lâmpada esteja apagada, verificaremos a temperatura e seguiremos a mesma lógica, identificando qual lâmpada cada interruptor acende.
