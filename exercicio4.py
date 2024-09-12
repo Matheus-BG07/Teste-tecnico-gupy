76bb9b36d15f8b26f7da6bf22d06bd32a5e92bb1
Matheus Borges <matheusbitcoin123@gmail.com>
2024-09-11 21:21:05 -0300

    Create exercicio4.py

---
+++ exercicio4.py
@@ -0,0 +1,14 @@
+# Lógica 1: Adicionar o número 2
+a = 9
+# Lógica 2: Dobrar o número anterior
+b = 128
+# Lógica 3: Elevação ao quadrado dos números à partir de 0
+c = 49
+# Lógica 4: Elevação ao quadrado dos números pares à partir de 2
+d = 100
+# Lógica 5: Sequência Fibonacci
+e = 13
+# Lógica 6: A lógica se inicia sobre a diferença entre os números, resultando: (8, 2, 4, 1), porém à partir do número 16, se adiciona o número 1, e esse padrão se repete algumas vezes ("16,17,18,19"), portanto o próximo número é 20
+f = 20
+
+print (f"As respostas são: \n a) {a} \n b) {b} \n c) {c} \n d) {d} \n e) {e} \n f) {f}")
